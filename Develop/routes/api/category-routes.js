94770a6256ef9b7b2dff9ffec26e84ba379cd050
LAPTOP-ELH40JFH\tessg <tess.guilmette@gmail.com>
2023-07-05 18:19:07 -0400

    finished get, post, put and delete functions for category-routes api

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -33,7 +33,7 @@ router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
-    const allCategories = await Category.findByPk(req.params.id, {
+    const oneCategory = await Category.findByPk(req.params.id, {
       include: [
         {
           model: Product,
@@ -42,7 +42,7 @@ router.get('/:id', async (req, res) => {
       ],
     });
 
-    const categories = allCategories.map((category) =>
+    const categories = oneCategory.map((category) =>
       category.get({ plain: true })
     );
 
@@ -55,16 +55,75 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const newCategory = await Category.create(req.body.category_name, {
+      include: [
+        {
+          model: Product,
+          attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+        },
+      ],
+    });
+
+    const createdCategory = newCategory.map((category) =>
+      category.get({ plain: true })
+    );
+
+    res.render('createdCategory', {
+      createdCategory,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const updatedCat = await Category.update(req.body, {
+      include: [
+        {
+          where: {
+          id: req.params.id
+          },
+        },
+      ],
+    });
+
+    const updatedItem = updatedCat.map((category) =>
+      category.get({ plain: true })
+    );
+
+    res.render('updatedItem', {
+      updatedItem,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No library card found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
